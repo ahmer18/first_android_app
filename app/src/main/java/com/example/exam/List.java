50a5ce19ce63318d6ace39be367d500baf092c17
Ahmer Sufwan Shakir <saffi203@gmail.com>
2023-05-01 14:45:33 +0500

    intial commit

---
+++ app/src/main/java/com/example/exam/List.java
@@ -0,0 +1,16 @@
+package com.example.exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class List extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
---
+++ app/src/main/java/com/example/exam/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.exam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+    }
+
+    public void Login() {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+}
